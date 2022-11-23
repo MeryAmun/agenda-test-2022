5aedba93e26f4b5712e2b10e0a1fa9ab6fc96c15
MeryAmun <chebesiawah@gmail.com>
2022-11-24 00:08:34 +0100

    added modal

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,58 +1,72 @@
-import React, { useState } from 'react';
-import { Table,Container } from 'react-bootstrap';
- 
-
-const now = new Date()
-const initialState = {
-title:'',
-description:'',
-status:'',
-date: now.getDate()/now.getMonth()/now.getFullYear(),
-time:now.toLocaleTimeString()
-}
-console.log(now.getDate(),now.getMonth() + 1,now.getFullYear(), now.toLocaleTimeString())
+import React, { useState } from "react";
+import { Table, Container } from "react-bootstrap";
+import { ModalComponent} from "./index";
 
 const HomeScreen = () => {
-    const [agenda, setAgenda] = useState(initialState);
+  const [agenda, setAgenda] = useState();
+  const [show, setShow] = useState(false);
 
+  
+  const handleShow = () => {
+    setShow(true)
+  }
 
-
+  const handleClose = () => {
+    setShow(false)
+};
   return (
-    <Container className='p-2 d-flex  flex-column justify-content-center align-items-center'>
-<div className="header m-4">
-    <h4>Agenda Application</h4>
-</div>
-<Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Status</th>
-          <th>Start Date</th>
-          <th>Completion Date</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>agenda Title</td>
-          <td>Agenda Description</td>
-          <td>done/pending</td>
-          <td>started on</td>
-          <td>completed on</td>
-          <td>
-            <button type='button' className='btn btn-primary m-1 border border-0'>Add</button>
-            <button type='button' className='btn btn-primary m-1 bg-danger border border-0'>Remove</button>
-            <button type='button' className='btn btn-primary m-1 bg-success border border-0'>Edit</button>
-          </td>
-        </tr>
-       
+    <Container className="p-2 d-flex  flex-column justify-content-center align-items-center">
+         <ModalComponent showModal={show} handleClose={handleClose}/>
+      <div className="header m-4">
+        <h4>Agenda Application</h4>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Start Date</th>
+            <th>Completion Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>agenda Title</td>
+            <td>Agenda Description</td>
+            <td>done/pending</td>
+            <td>started on</td>
+            <td>completed on</td>
+            <td>
+              <button
+                type="button"
+                className="btn btn-primary m-1 border border-0"
+                onClick={handleShow}
+              >
+                Add
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary m-1 bg-danger border border-0"
+              >
+                Remove
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary m-1 bg-success border border-0"
+              >
+                Edit
+              </button>
+            </td>
+          </tr>
+
           {/* <td colSpan={2}>Larry the Bird</td>
           <td colSpan={2}>Larry the Bird</td> */}
-      </tbody>
-    </Table>
+        </tbody>
+      </Table>
     </Container>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Form, Button, Modal } from 'react-bootstrap'
 
-const Modal = () => {
+
+const now = new Date()
+const initialState = {
+title:'',
+description:'',
+status:'',
+date: now.getDate()/now.getMonth()/now.getFullYear(),
+time:now.toLocaleTimeString()
+}
+console.log(now.getDate(),now.getMonth() + 1,now.getFullYear(), now.toLocaleTimeString());
+
+
+const ModalComponent = ({showModal, handleClose}) => {
+    const [data, setData] = useState(initialState)
+   
+    
+
+
   return (
-    <div>Modal</div>
+    <>
+    <Modal show={showModal} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              autoFocus
+            />
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </>
   )
 }
 
-export default Modal
+export default ModalComponent
